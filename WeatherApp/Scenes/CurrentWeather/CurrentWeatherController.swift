bd08e5f45ddf3678a0b57bda00c43297d06e32c6
Vato Kostava <vkost16@freeuni.edu.ge>
2021-08-20 20:10:27 +0400

    Add separator lines to weather details

--- WeatherApp/Scenes/CurrentWeather/CurrentWeatherController.swift
+++ WeatherApp/Scenes/CurrentWeather/CurrentWeatherController.swift
@@ -20,6 +20,8 @@ class CurrentWeatherController: UIViewController {
     @IBOutlet private weak var pressureLabel: UILabel!
     @IBOutlet private weak var windSpeedLabel: UILabel!
     @IBOutlet private weak var windDirectionLabel: UILabel!
+    @IBOutlet private weak var topSeparatorLine: UIView!
+    @IBOutlet private weak var bottomSeparatorLine: UIView!
     
     var locationManager: CLLocationManager = .init()
     
@@ -33,8 +35,30 @@ extension CurrentWeatherController {
     override func viewDidLoad() {
         super.viewDidLoad()
         CurrentWeatherConfiguratorImpl().configure(self)
+        setupSeparatorLines()
         setupLocationManager()
         presenter.viewDidLoad()
+
+    }
+    
+}
+
+// MARK: Setup
+extension CurrentWeatherController {
+    
+    private func setupSeparatorLines() {
+        drawDottedLine(
+            start: CGPoint(x: topSeparatorLine.bounds.minX, y: topSeparatorLine.bounds.minY),
+            end: CGPoint(x: topSeparatorLine.bounds.maxX, y: topSeparatorLine.bounds.minY),
+            view: topSeparatorLine,
+            color: .gray
+        )
+        drawDottedLine(
+            start: CGPoint(x: bottomSeparatorLine.bounds.minX, y: bottomSeparatorLine.bounds.minY),
+            end: CGPoint(x: bottomSeparatorLine.bounds.maxX, y: bottomSeparatorLine.bounds.minY),
+            view: bottomSeparatorLine,
+            color: .gray
+        )
     }
     
 }
@@ -85,6 +109,23 @@ extension CurrentWeatherController: LocationAccessible {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         presenter.didUpdateLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+    }
+    
+}
+
+// MARK: Dotted line draw helper
+extension CurrentWeatherController {
+    
+    func drawDottedLine(start p0: CGPoint, end p1: CGPoint, view: UIView, color: UIColor) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = 2
+        shapeLayer.lineDashPattern = [3, 3]
+        
+        let path = CGMutablePath()
+        path.addLines(between: [p0, p1])
+        shapeLayer.path = path
+        view.layer.addSublayer(shapeLayer)
     }
     
 }
